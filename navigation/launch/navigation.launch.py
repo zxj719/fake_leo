63fa599c6a6275de4ae19fd83f1ca327662b2c12
Xingjian Zhang <146145387+zxj719@users.noreply.github.com>
2024-04-19 00:53:21 +0100

    Add files via upload

---
+++ navigation/launch/navigation.launch.py
@@ -0,0 +1,46 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
+from launch_ros.actions import Node, SetParameter
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+import xacro
+
+def generate_launch_description():
+    ld = LaunchDescription()
+    pkg_name = 'navigation'
+
+    # LOAD PARAMETERS FROM YAML FILES
+    joy_config = PathJoinSubstitution([get_package_share_directory(pkg_name), 'config', 'joy_config.yaml'])
+
+    launch_node_joy = Node(
+        package="joy",
+        executable="joy_node",
+        name="joy_node",
+        parameters=[
+            {"dev": "/dev/input/js0"},
+            {"coalesce_interval": "0.02"},
+            {"autorepeat_rate": "30.0"},
+        ],
+    )
+
+    launch_node_joy_teleop = Node(
+        package="teleop_twist_joy",
+        executable="teleop_node",
+        name="joy_teleop_node",
+        remappings=[("/cmd_vel", "cmd_vel")],
+        parameters=[joy_config],
+    )
+
+    # Include SLAM Toolbox standard launch file
+    lidar = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource([get_package_share_directory('rplidar_ros'), '/launch', '/rplidar_a2m12_launch.py']),
+    launch_arguments={}.items(),
+    )
+
+    ld.add_action(launch_node_joy)
+    ld.add_action(launch_node_joy_teleop)
+    ld.add_action(lidar)
+
+    return ld
